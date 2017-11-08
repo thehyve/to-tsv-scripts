20ddb0ee8c31cf9fc966f8eebd27916b75de8d83
Ruslan Forostianov <ruslan@thehyve.nl>
2017-11-08 16:27:35 +0100

    Let jackcess detect the encoding of the access db file.
    It writes to UTF-8 anyway.

--- access_to_tsv.groovy
+++ access_to_tsv.groovy
@@ -1,12 +1,8 @@
-package extract
-
 @Grab(group = 'com.healthmarketscience.jackcess', module = 'jackcess', version = '2.1.8')
 import com.healthmarketscience.jackcess.Database
 import com.healthmarketscience.jackcess.DatabaseBuilder
 import com.healthmarketscience.jackcess.util.ExportUtil
 import com.healthmarketscience.jackcess.util.SimpleExportFilter
-
-import java.nio.charset.StandardCharsets
 
 assert args && args.length <= 2: 'usage: access_to_tsv <access database file> [<tsv output directory>]'
 
@@ -24,10 +20,6 @@ boolean header = true
 String delimiter = '\t'
 char quote = '"'
 String fileExt = 'tsv'
-//TODO Ensure UTF-8. We need to know. jackcess seems to detect the encoding from the file. Otherwise we can set encoding with:
-//db.setCharset()
-//TODO Do conversion?
-assert db.charset == StandardCharsets.UTF_8
 db.tableNames.each { String tableName ->
     File file = new File(outputDir, "${tableName}.${fileExt}")
     println "Exporting ${tableName} table to ${file}."
